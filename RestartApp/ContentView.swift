47fdc43a316b5fe1cfb6fd90879fbedc7e8ebdd9
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-17 13:37:58 +0530

    Added Image extention to refactor code and addressed different pphases of image view and gonna display image accordingly

--- RestartApp/ContentView.swift
+++ RestartApp/ContentView.swift
@@ -1,25 +1,51 @@
 import SwiftUI
 
+// added an Image extention that can be used by image views
+extension Image{
+    func imageModifier() -> some View{
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View{
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
     
-    private let imageURL:String = "https://credo.academy/credo-academy@3x.png"
+    private let imageURL:String = "https://credo.academy/credo-academy@3x.pn"
     
     var body: some View {
         
-        // AsyncImage(url: URL(string: imageURL)) // This line generates image which fills the entire screen
+        //        AsyncImage(url: URL(string: imageURL)) { image in
+        //            image
+        //                .imageModifier()
+        //        } placeholder: {
+        //            Image(systemName: "photo.circle.fill")
+        //                .iconModifier()
+        //        }
+        //        .padding(40) // as this value increases the image gets shrinked and shrinked on screen
+        //    }
         
-        // AsyncImage(url: URL(string: imageURL), scale: 3.0) // default value of scale is 1.0, we can make image smaller by increasing value or larger by decreasing its value via this scale parameter
         
-        AsyncImage(url: URL(string: imageURL)) { image in image
-                .resizable()
-                .scaledToFit()
-        } placeholder: { // when image URL is wrong or internet is off.. instead of actual image this custom placeholder will be displayed when image is not loaded or renderd
-            Image(systemName: "photo.circle.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 128)
-                .foregroundColor(.purple)
-                .opacity(0.5)
+        AsyncImage(url: URL(string: imageURL)){
+            phase in // phase gives current information of same
+            // SUCCESS : image is successfully loaded
+            // FAILURE : image load failed with an error
+            // EMPTY : No image is loaded
+            if let image = phase.image{ // success stage
+                image.imageModifier()
+            } else if phase.error != nil{
+                Image(systemName: "ant.circle.fill").iconModifier() //failure state i.e when we send wrong url
+            }else {
+                Image(systemName: "photo.circle.fill").iconModifier() // empty state.. like at start when image is gonna be displayed on UI
+            }
         }
         .padding(40)
     }
