eed7d45dfe6fbedbbac3c89caedccd9b20a5e355
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-17 12:42:54 +0530

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Usha Sai Chintha on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
