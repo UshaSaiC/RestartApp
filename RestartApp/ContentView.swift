e51cf4fc0b881bad770684ca6b75e7558faf2535
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-17 13:52:17 +0530

    Added animations for image view based on phase of image

--- RestartApp/ContentView.swift
+++ RestartApp/ContentView.swift
@@ -19,32 +19,22 @@ extension Image{
 
 struct ContentView: View {
     
-    private let imageURL:String = "https://credo.academy/credo-academy@3x.pn"
+    private let imageURL:String = "https://credo.academy/credo-academy@3x.png"
     
     var body: some View {
-        
-        //        AsyncImage(url: URL(string: imageURL)) { image in
-        //            image
-        //                .imageModifier()
-        //        } placeholder: {
-        //            Image(systemName: "photo.circle.fill")
-        //                .iconModifier()
-        //        }
-        //        .padding(40) // as this value increases the image gets shrinked and shrinked on screen
-        //    }
-        
-        
-        AsyncImage(url: URL(string: imageURL)){
-            phase in // phase gives current information of same
-            // SUCCESS : image is successfully loaded
-            // FAILURE : image load failed with an error
-            // EMPTY : No image is loaded
-            if let image = phase.image{ // success stage
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
                 image.imageModifier()
-            } else if phase.error != nil{
-                Image(systemName: "ant.circle.fill").iconModifier() //failure state i.e when we send wrong url
-            }else {
-                Image(systemName: "photo.circle.fill").iconModifier() // empty state.. like at start when image is gonna be displayed on UI
+                    // .transition(.move(edge: .bottom)) // the image will be coming from bottom of screen
+                    // .transition(.slide) // image comes from left to middle
+                    .transition(.scale) // image size increases from small to big in an instant
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
             }
         }
         .padding(40)
